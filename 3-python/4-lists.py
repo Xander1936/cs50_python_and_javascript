94aa492168ded39b5ab37a7026292442f1f852ba
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-05-04 17:09:02 +0100

    Lesson 3: python - lists

---
+++ 3-python/4-lists.py
@@ -0,0 +1,11 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+
+# Add a value to the end of an existing list
+names.append("Draco")
+
+# Sort the list and modify the order
+names.sort()
+
+print(names)
