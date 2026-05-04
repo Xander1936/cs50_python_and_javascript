cbf2ad057a0d655a4934b09da943ccf3742701cf
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-05-04 17:06:59 +0100

    Lesson 3: python - sequences: string, list, tuple (with their coordinates)

---
+++ 3-python/3-sequences.py
@@ -0,0 +1,20 @@
+# Set - collection of unique values like mathematic values
+# dict - collection of key-value pairs
+
+# Show the characters of this sequence
+name = "Harry Gryffindor"
+
+print(name[1])
+
+# list - Sequence of mutable values
+# Show the elements in the list called names
+names = ["Harry", "Ron", "Hermione", "Draco"]
+
+print(names[2])
+
+# Tuple - sequence of immutable values
+#  Coordinates
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
