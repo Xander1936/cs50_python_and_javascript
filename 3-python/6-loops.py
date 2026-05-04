1f98f55d945bb3b4bf312ea1bef16797736d8805
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-05-04 17:29:30 +0100

    Lesson 3: python - loops

---
+++ 3-python/6-loops.py
@@ -0,0 +1,21 @@
+# Loop each element in the list, called it "i" and print it
+# for i in [0, 1, 2, 3, 4, 5]:
+    # print(i)
+    
+# For Thousand numbers, you can use a range to loop throw a list
+for i in range(6):
+    print(i)
+    
+# Loop throw a list of names
+names = ["Ron", "Hermione", "Harry", "Draco" ]
+
+for name in names:
+    print(name)
+
+# print(names)
+
+# Loop throw characters in a name
+name = "Harry"
+
+for character in name:
+    print(character) 
