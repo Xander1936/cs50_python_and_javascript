fd222be584dfba3a0b7998fcc926d2d0dba17959
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-05-04 17:13:59 +0100

    Lesson 3: python - sets

---
+++ 3-python/5-sets.py
@@ -0,0 +1,17 @@
+# Create an empty set
+s = set()
+
+# Add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(3)
+
+# Remove 2 from the set
+s.remove(2)
+print(s)
+
+# len Gives the length of an element: sequence, set or list
+print(f"The set has {len(s)} elements.")
