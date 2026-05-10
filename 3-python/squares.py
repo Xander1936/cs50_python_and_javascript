169ecc4db2bb4b4f55a2453a22b5bf3ce60632fd
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-05-10 19:49:24 +0100

    Lesson 3: python - functions_dec_and_import

---
+++ 3-python/squares.py
@@ -0,0 +1,9 @@
+# import functions
+from functions import square
+
+# for i in range(10):
+    # print(f"The square of {i} is {square(i)}.")
+
+for i in range(10):
+    # print(f"The square of {i} is {functions.square(i)}") 
+    print(f"The square of {i} is {square(i)}") 
