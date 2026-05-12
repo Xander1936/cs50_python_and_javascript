21ad74f3f43bc8a2543683d3eecab4b6b196721d
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-05-12 17:15:12 +0100

    Lesson 4: python - django - session_storage_capacity_of_session

--- 4-django/lecture3/tasks/views.py
+++ 4-django/lecture3/tasks/views.py
@@ -13,13 +13,14 @@ from django.urls import reverse
 class NewTaskForm(forms.Form):
     # Task label
     task = forms.CharField(label="New Task")
-    # Priority is a new field added
-    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    # Client side validation: Priority is a new field added
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
     # Create a session for each user with its own data
     if "tasks" not in request.session:
+        # Create an empty list if the user don't have tasks on his session
         request.session["tasks"] = []
     # Render a template index.html and provide a context {}
     return render(request, "tasks/index.html", {
@@ -31,17 +32,19 @@ def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
+            # form.cleaned_data help me to get all the data 
+            # the user submitted and save it in a variable called task 
             task = form.cleaned_data["task"]
             # Append to the list of the Tasks
             # tasks.append(task)
             request.session["tasks"] += [task]
-            # Redirect the user to the Task List Page
+            # Redirect the user to the Task List Page after adding the task
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
             })
-            
+    # Render an empty form if the user just want to get the page.       
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
